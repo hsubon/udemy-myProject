d7e5c8afa0e60af95ac2ea97920713e53e6d244d
hsubon <sonygs888@gmail.com>
2019-05-19 11:36:49 +0800

    test

--- Magic/main.js
+++ Magic/main.js
@@ -1,4 +1,4 @@
-const path = 'C:/Users/User/Documents/GitHub/udemy-myProject/Magic/img/';
+const path = 'img/';
 
 let start = document.getElementById('start');
 let container = document.getElementsByClassName('container')[0];
